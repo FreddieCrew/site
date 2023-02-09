726e8be85b563873a19acd19f4a2f77ce6305382
FreddieCrew <120303127+FreddieCrew@users.noreply.github.com>
2023-02-09 06:08:02 -0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,3 +1,7 @@
 function Redirect() { 
     window.open("https://www.github.com/FreddieCrew", "Git"); 
 }   
+
+function Disablepopupdueaerror() {
+    document.getElementById('popup').style.display = "none";
+}
